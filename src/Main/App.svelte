ce0c2d63c0c1c001a52388a85ecc40fb7f3c44d8
John Henrik Muller <jhmu@itu.dk>
2020-09-30 13:47:44 +0200

    More quotes

--- src/Main/App.svelte
+++ src/Main/App.svelte
@@ -44,6 +44,9 @@ const todo = [
 const quotes = [
 	{source: "Aiki paper", text: "Based on these different outcomes, future implementations of the extension may consider different, specialized versions which cater to different types of users. Currently, Aiki is designed as an ultra-simple one-fits-all solution, put specializations are worth exploring: for instance design with a focus on promoting awareness and/or reflection a focus on distracting from procrastination behavior, or a focus on maximizing for engagement with the learning tasks."},
 	{source: "Aiki paper", text: "When inquiring about this further in this and other participants’ post-study interviews, we discussed a design goal of designing the extension / learning environment to provide positive feedback in order to create positive associations with the microlearning tasks."},
+	{source: "Aiki paper", text: "Future designs of similar extensions might consider implementing modes, such as a focus-mode and a leisure-mode (either automatically detected or manually controlled), where specific websites are not intercepted."},
+	{source: "Aiki paper", text: "One way to mitigate this is to explore a design where the microlearning opportunity appears as a window-overlay on top of the time-wasting site."},
+	{source: "Aiki paper", text: "This suggests that task-based interceptions may make more sense to some users."},
 	{source: "Zeeguu RP", text:'“(...) when people reflexively turn to a device—increasingly a smartphone—to act on a need to learn something, do something, discover something, watch something, or buy something.” (Ramaswamy, 2015).'}
 ]
 
